231f09b4b68f7611a47b1cf2c1b474f05c0a008b
Eric Hunzeker <emh128@pitt.edu>
2019-04-26 15:59:30 -0400

    finished spark

--- spark inverted index/Driver.scala
+++ spark inverted index/Driver.scala
@@ -5,44 +5,30 @@ import org.apache.spark.sql.hive.HiveContext
 
 object Driver {
   def main(args: Array[String]) {
-    val conf = new SparkConf().setAppName("Spark - inverted Index")
-    val sc = new SparkContext(conf)
-
-    // Read file containing stopwords into a broadcast variable
-    val stopWordsInput = sc.textFile(args(0))
-    val stopWords = stopWordsInput.flatMap(x => x.split("\\r?\\n")).map(_.trim)
-    val broadcastStopWords = sc.broadcast(stopWords.collect.toSet)
-
+    val conf = new SparkConf().setAppName("inverted index")
 
     // Read input file and filter all stopwords
-    sc.wholeTextFiles(args(1)).flatMap {
+    sc.wholeTextFiles("/user/chatree/project/docs/").flatMap {
       case (path, text) =>
         text.replaceAll("[^\\w\\s]|('s|ly|ed|ing|ness) ", " ")
           .split("""\W+""")
-          .filter(!broadcastStopWords.value.contains(_)) map {
-          // Create a tuple of (word, filePath)
+          .map {
             word => (word, path)
           }
     }.map {
-      // Create a tuple with count 1 ((word, fileName), 1)
       case (w, p) => ((w, p.split("/")(6)), 1)
     }.reduceByKey {
-      // Group all (word, fileName) pairs and sum the counts
       case (n1, n2) => n1 + n2
     }.map {
-      // Transform tuple into (word, (fileName, count))
       case ((w, p), n) => (w, (p, n))
     }.groupBy {
-      // Group by words
       case (w, (p, n)) => w
     }.map {
-      // Output sequence of (fileName, count) into a comma seperated string
       case (w, seq) =>
         val seq2 = seq map {
           case (_, (p, n)) => (p, n)
         }
         (w, seq2.mkString(", "))
-    }.saveAsTextFile(args(2))
+    }.saveAsTextFile("./sparkoutput")
   }
-
 }
